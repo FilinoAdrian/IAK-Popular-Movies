d4ee81810e1c6b0ca3733994e87395ad88c1029d
Filino <filino.adrian@gmail.com>
2018-02-07 13:51:52 +0700

    Implement SwipeRefreshLayout

--- app/src/main/java/com/example/android/iak_popular_movies/MainActivity.java
+++ app/src/main/java/com/example/android/iak_popular_movies/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.iak_popular_movies;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,11 +20,30 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private MovieAdapter mMovieAdapter;
+    private SwipeRefreshLayout swipeContainer;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        swipeContainer = (SwipeRefreshLayout)findViewById(R.id.main_content);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initViews();
+            }
+        });
+    }
+
+    public void initViews(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
@@ -67,6 +88,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(ArrayList<Movie> movies) {
+            if (swipeContainer.isRefreshing()){
+                swipeContainer.setRefreshing(false);
+            }
+            progressDialog.dismiss();
             mMovieAdapter.setMovieData(movies);
         }
     }
